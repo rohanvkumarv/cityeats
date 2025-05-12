6dd9131b45b24e6b1e9dd80c392ed3b4eee74c01
Rohan Kumar <rohanvkumarv@gmail.com>
2025-05-12 13:02:21 +0530

    done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,45 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+
+{
+  "extends": [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "rules": {
+    // Disable the unescaped entities rule since these are handled by React
+    "react/no-unescaped-entities": "off",
+    
+    // Disable the img element warning if you don't want to use next/image
+    "@next/next/no-img-element": "off",
+    
+    // Set exhaustive-deps to warn instead of error
+    "react-hooks/exhaustive-deps": "warn",
+    
+    // Disable undefined components error if you're using motion from framer-motion
+    "react/jsx-no-undef": ["error", { "allowGlobals": true }],
+    
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
