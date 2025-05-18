f748fb6f51ae8a71534c476d6dbabfd780864a4d
Rohan Kumar <rohanvkumarv@gmail.com>
2025-05-18 07:29:07 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,19 @@
-// 
+// // 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   typescript: {
+//     // ⚠️ Dangerously allow production builds to successfully complete even if
+//     // your project has type errors.
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     // ⚠️ Also ignore ESLint errors during builds if you have any
+//     ignoreDuringBuilds: true,
+//   },
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,6 +25,23 @@ const nextConfig = {
   eslint: {
     // ⚠️ Also ignore ESLint errors during builds if you have any
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'workongigs.s3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Wildcard pattern for any S3 bucket
+      {
+        protocol: 'https',
+        hostname: '*.s3.*.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
